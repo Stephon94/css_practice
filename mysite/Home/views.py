cc4e3e1f11fb7d607c13c5e9f2cf44859b3b1063
Stephon <micronixon30@gmail.com>
2018-03-23 05:02:45 -0400

    secondcommit

--- mysite/Home/views.py
+++ mysite/Home/views.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.core.cache import cache
 
 def index(request):
+	cache.clear()
 	return render(request,'index.html')
